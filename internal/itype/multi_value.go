1b903f220de951ba4c438e310ea328318401da1b
萤火 <luna@cyl-mail.com>
2024-12-04 10:50:55 +0800

    add multi value

---
+++ internal/itype/multi_value.go
@@ -0,0 +1,54 @@
+package itype
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"github.com/Luna-CY/Golang-Project-Template/internal/errors"
+)
+
+type MultiValues[T any] []T
+
+func (cls *MultiValues[T]) Scan(value any) error {
+	if s, ok := value.(string); ok {
+		if "" == s {
+			*cls = make(MultiValues[T], 0)
+
+			return nil
+		}
+
+		if err := json.Unmarshal([]byte(s), cls); nil != err {
+			return err
+		}
+
+		return nil
+	}
+
+	if bs, ok := value.([]byte); ok {
+		if 0 == len(bs) {
+			*cls = make(MultiValues[T], 0)
+
+			return nil
+		}
+
+		if err := json.Unmarshal(bs, cls); nil != err {
+			return err
+		}
+
+		return nil
+	}
+
+	return errors.New("invalid values")
+}
+
+func (cls *MultiValues[T]) Value() (driver.Value, error) {
+	if nil == cls || 0 == len(*cls) {
+		return "[]", nil
+	}
+
+	var value, err = json.Marshal(cls)
+	if nil != err {
+		return nil, err
+	}
+
+	return string(value), nil
+}
